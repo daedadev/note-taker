9576708996addd638796e18fdc9d1aa74ea2e111
Daniel Moore <wron2@hotmail.com>
2021-05-04 09:40:13 -0700

    changing some

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const api = require('./routes/api-routes')
-const htmlRoute = require('./routes/html-routes')
-  
-const app = express()
-  
-const port = 3000
-  
-// The body-parser middleware 
+const express = require("express");
+const api = require("./routes/api-routes");
+const htmlRoute = require("./routes/html-routes");
+
+const app = express();
+
+const port = 3001;
+
+// The body-parser middleware
 // to parse form data
 app.use(express.json());
 app.use("/api", api);
@@ -14,8 +14,7 @@ app.use("/", htmlRoute);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 // Server setup
 app.listen(port, () => {
-    console.log(`Server start on port ${port}`)
-  })
+  console.log(`Server start on port ${port}`);
+});
